43c44bb173d1f5857415e8d99067ec99e26e51ce
Marcos Coelho <mparav124@gmail.com>
2023-02-27 09:20:47 -0300

    fix: anima numeros refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
@@ -22,7 +22,12 @@ export default function initFetchAnimais() {
         numerosGrid.appendChild(divAnimais);
       });
 
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
