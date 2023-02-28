4d80c6e6d3476455c6dd8541e6034028219831df
Marcos Coelho <mparav124@gmail.com>
2023-02-28 09:00:00 -0300

    fix: fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ import Tabnav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import fetchAnimais from "./modules/fetch-animais.js";
+import fetchBitcoin from "./modules/fetch-bitcoin.js";
 import initScrollAnimado from "./modules/scroll-animacao.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -34,8 +34,9 @@ tooltip.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 
+fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
+
 initScrollAnimado();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
